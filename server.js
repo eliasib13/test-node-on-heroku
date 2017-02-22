e15a627e0623573e565cbe9ff9bbd023560b8705
Eliasib García <eliasib.lv.12@gmail.com>
2017-02-22 23:01:19 +0000

    First commit.
    - Configured simple HTTP server
    - Added npm info on package.json
    - Added .gitignore to untrack some files
    - Added .eslintrc file with options for ESLint

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+function onRequest(request, response) {
+    console.log('[function]onRequest: Start');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('Hello world!');
+    response.end();
+}
+
+let port = 9999;
+http.createServer(onRequest).listen(port);
+console.log(`Server started. Listening on port ${port}`);
